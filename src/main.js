71728a650b9d0e32e84dae8dde3e4094f74d1a01
Matthias Bruns <git@matthiasbruns.com>
2025-05-09 08:45:44 +0200

    feat: add all code

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { setupCountdown } from './countdown.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
+    <a href="https://heyzine.com/flip-book/c02fe7fc75.html" target="_blank">
       <img src="${piaLogo}" class="logo" alt="Vite logo" />
     </a>
   </div>
@@ -12,6 +12,6 @@ document.querySelector('#app').innerHTML = `
 `
 
 // Countdown setup
-const targetDate = new Date('2024-05-22T12:00:00'); // Set your target date here
+const targetDate = new Date('2025-05-22T12:00:00'); // Set your target date here
 const countdownContainer = document.getElementById('countdown-container');
 setupCountdown(targetDate, countdownContainer);
